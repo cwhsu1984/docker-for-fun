b2692511f19ddbb9e70e3e0dd2d688d649d254d3
cwhsu <cwhsu1984@gmail.com>
2019-01-15 23:55:08 +0800

    Start a nginx php-fpm website

---
+++ web/fpm/run.sh
@@ -0,0 +1,12 @@
+# build php-fpm docker image with tag "fpm:7.2"
+sudo docker build -t fpm:7.2 .
+
+# user defined network
+sudo docker network create mynet
+
+# run a container named "fpm" with image "fpm:7.2"
+sudo docker run --name fpm -d \
+    -p ":9000" \
+    -v /home/cwhsu/docker-demo/web/fpm/www:/usr/share/nginx/html \
+    --network mynet \
+    fpm:7.2
--- web/nginx/run.sh
+++ web/nginx/run.sh
@@ -2,4 +2,5 @@
 sudo docker build -t nginx:0.1 .
 
 # run a container named "myweb" with image "nginx:0.1" on localhost:8080
-sudo docker run --name myweb -d -p 8080:80 -v /home/cwhsu/docker-demo/web/nginx/www:/usr/share/nginx/html -d nginx:0.1
+sudo docker run --name myweb -d -p 8080:80 \
+    --network mynet -d nginx:0.1
